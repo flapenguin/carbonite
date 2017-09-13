7b61f51d053fc922c31f56f27534fcb71c148bf5
Andrey Roenko <flapenguin@yandex-team.ru>
2017-09-13 15:36:26 +0300

    use UMD

--- dist/carbonite.js
+++ dist/carbonite.js
@@ -1,5 +1,14 @@
-var carbonite =
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["carbonite"] = factory();
+	else
+		root["carbonite"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -61,7 +70,7 @@ var carbonite =
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 5);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -102,12 +111,145 @@ function extractVersion(regex) {
 "use strict";
 
 exports.__esModule = true;
-var Resource_1 = __webpack_require__(2);
-var inlineStyles_1 = __webpack_require__(5);
-var withLoadedImage_1 = __webpack_require__(6);
-var StyleSheet_1 = __webpack_require__(8);
+var browser = __webpack_require__(0);
+exports.URL = window.URL
+    || window.webkitURL
+    || window.mozURL
+    || window;
+exports.isSupported = function (csp) {
+    // IE doesn't support foreignObject.
+    return !browser.isIE
+        // TODO: test on mobile browsers.
+        && !browser.isMobile
+        // No way to make Firefox load svg with styles inside into <img> if
+        // we don't have 'nonce-...' in style-src.
+        // TODO: maybe detect style-src 'unsafe-inline' somehow and then use
+        // style attributes?
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=1358106
+        && !(browser.isFirefox && csp.enabled && !csp.styleNonce)
+        && typeof XMLSerializer !== 'undefined';
+};
+exports.isNonSvgSupported = function (csp) {
+    return exports.isSupported(csp)
+        // Safari and old versions of Opera have trouble rendering foreignObject.
+        && !browser.isSafari
+        && !browser.isOldOpera
+        // Edge silently fails to drawImage SVG after onload.
+        // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/4411619/
+        && !browser.isEdge;
+};
+exports.areBlobsSupported = function (csp) {
+    return typeof Blob !== 'undefined'
+        && exports.URL.createObjectURL
+        // Safari doesn't want to load SVG blobs with foreignObject inside.
+        // Also, Safari can't render anything, so blobs are useless for a canvas too.
+        && !browser.isSafari;
+};
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+exports.__esModule = true;
+function cssomKeyToCssKey(cssomKey) {
+    if (cssomKey === 'cssFloat') {
+        return 'float';
+    }
+    return cssomKey
+        .replace(/([A-Z])/g, '-$&')
+        .replace(/^(webkit|ms|moz)/, '-')
+        .toLowerCase();
+}
+exports.cssomKeyToCssKey = cssomKeyToCssKey;
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+exports.__esModule = true;
+var parent = function () { return document.body; };
+var container = null;
+var refCount = 0;
+function append(node) {
+    refCount++;
+    if (!container) {
+        container = document.createElement('div');
+        container.style.position = 'absolute';
+        container.style.left = '-99999px';
+        container.style.top = '-99999px';
+        parent().appendChild(container);
+    }
+    container.appendChild(node);
+    var savedContainer = container;
+    var disposed = false;
+    return {
+        dispose: function () {
+            if (disposed) {
+                return;
+            }
+            disposed = true;
+            savedContainer.removeChild(node);
+            if (--refCount === 0) {
+                savedContainer.parentElement.removeChild(savedContainer);
+                container = null;
+            }
+        }
+    };
+}
+exports.append = append;
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+exports.__esModule = true;
+var temporaryDom = __webpack_require__(3);
+function withLoadedImage(url, callback) {
+    var img = new Image();
+    var tempDom = temporaryDom.append(img);
+    var loaded = new Promise(function (resolve, reject) {
+        img.onload = function () { return resolve(); };
+        img.onerror = function () {
+            var shortUrl = url.length <= 64 ? url : url.substr(0, 61) + '...';
+            reject(new Error("carbonite: failed to load image " + shortUrl));
+        };
+        img.src = url;
+    });
+    return loaded
+        .then(function () { return callback(img); })
+        .then(function (value) {
+        tempDom.dispose();
+        return value;
+    }, function (error) {
+        tempDom.dispose();
+        return Promise.reject(error);
+    });
+}
+exports.withLoadedImage = withLoadedImage;
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+exports.__esModule = true;
+var Resource_1 = __webpack_require__(6);
+var inlineStyles_1 = __webpack_require__(8);
+var withLoadedImage_1 = __webpack_require__(4);
+var StyleSheet_1 = __webpack_require__(9);
 var htmlToSvg_1 = __webpack_require__(10);
-var support_1 = __webpack_require__(3);
+var support_1 = __webpack_require__(1);
 function render(node, options) {
     var csp = options.csp || { enabled: false };
     var nonSvgSupported = support_1.isNonSvgSupported(csp);
@@ -153,14 +295,14 @@ function rasterizeSvg(svg, options) {
 
 
 /***/ }),
-/* 2 */
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 exports.__esModule = true;
-var support_1 = __webpack_require__(3);
-var base64 = __webpack_require__(4);
+var support_1 = __webpack_require__(1);
+var base64 = __webpack_require__(7);
 var browser = __webpack_require__(0);
 exports.defaultType = 'blob';
 function fromDataUrl(url) {
@@ -237,50 +379,7 @@ exports.getResourceTypeForForeignObjectSvg = getResourceTypeForForeignObjectSvg;
 
 
 /***/ }),
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-exports.__esModule = true;
-var browser = __webpack_require__(0);
-exports.URL = window.URL
-    || window.webkitURL
-    || window.mozURL
-    || window;
-exports.isSupported = function (csp) {
-    // IE doesn't support foreignObject.
-    return !browser.isIE
-        // TODO: test on mobile browsers.
-        && !browser.isMobile
-        // No way to make Firefox load svg with styles inside into <img> if
-        // we don't have 'nonce-...' in style-src.
-        // TODO: maybe detect style-src 'unsafe-inline' somehow and then use
-        // style attributes?
-        // https://bugzilla.mozilla.org/show_bug.cgi?id=1358106
-        && !(browser.isFirefox && csp.enabled && !csp.styleNonce)
-        && typeof XMLSerializer !== 'undefined';
-};
-exports.isNonSvgSupported = function (csp) {
-    return exports.isSupported(csp)
-        // Safari and old versions of Opera have trouble rendering foreignObject.
-        && !browser.isSafari
-        && !browser.isOldOpera
-        // Edge silently fails to drawImage SVG after onload.
-        // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/4411619/
-        && !browser.isEdge;
-};
-exports.areBlobsSupported = function (csp) {
-    return typeof Blob !== 'undefined'
-        && exports.URL.createObjectURL
-        // Safari doesn't want to load SVG blobs with foreignObject inside.
-        // Also, Safari can't render anything, so blobs are useless for a canvas too.
-        && !browser.isSafari;
-};
-
-
-/***/ }),
-/* 4 */
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -300,16 +399,16 @@ exports.encode = encode;
 
 
 /***/ }),
-/* 5 */
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 exports.__esModule = true;
-var cssomKeyToCssKey_1 = __webpack_require__(9);
+var cssomKeyToCssKey_1 = __webpack_require__(2);
 var browser = __webpack_require__(0);
-var temporaryDom = __webpack_require__(7);
-var withLoadedImage_1 = __webpack_require__(6);
+var temporaryDom = __webpack_require__(3);
+var withLoadedImage_1 = __webpack_require__(4);
 // Css rules that cannot be cut off.
 // transform-origin is buggy in Chromiums.
 var forcedStyles = /^transform-origin^/;
@@ -455,84 +554,13 @@ function loadImageAsDataUrl(node) {
 
 
 /***/ }),
-/* 6 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 exports.__esModule = true;
-var temporaryDom = __webpack_require__(7);
-function withLoadedImage(url, callback) {
-    var img = new Image();
-    var tempDom = temporaryDom.append(img);
-    var loaded = new Promise(function (resolve, reject) {
-        img.onload = function () { return resolve(); };
-        img.onerror = function () {
-            var shortUrl = url.length <= 64 ? url : url.substr(0, 61) + '...';
-            reject(new Error("carbonite: failed to load image " + shortUrl));
-        };
-        img.src = url;
-    });
-    return loaded
-        .then(function () { return callback(img); })
-        .then(function (value) {
-        tempDom.dispose();
-        return value;
-    }, function (error) {
-        tempDom.dispose();
-        return Promise.reject(error);
-    });
-}
-exports.withLoadedImage = withLoadedImage;
-
-
-/***/ }),
-/* 7 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-exports.__esModule = true;
-var parent = function () { return document.body; };
-var container = null;
-var refCount = 0;
-function append(node) {
-    refCount++;
-    if (!container) {
-        container = document.createElement('div');
-        container.style.position = 'absolute';
-        container.style.left = '-99999px';
-        container.style.top = '-99999px';
-        parent().appendChild(container);
-    }
-    container.appendChild(node);
-    var savedContainer = container;
-    var disposed = false;
-    return {
-        dispose: function () {
-            if (disposed) {
-                return;
-            }
-            disposed = true;
-            savedContainer.removeChild(node);
-            if (--refCount === 0) {
-                savedContainer.parentElement.removeChild(savedContainer);
-                container = null;
-            }
-        }
-    };
-}
-exports.append = append;
-
-
-/***/ }),
-/* 8 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-exports.__esModule = true;
-var cssomKeyToCssKey_1 = __webpack_require__(9);
+var cssomKeyToCssKey_1 = __webpack_require__(2);
 function joinRules(rules) {
     var parts = [];
     // tslint:disable-next-line:forin
@@ -561,25 +589,6 @@ exports.StyleSheet = StyleSheet;
 
 
 /***/ }),
-/* 9 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-exports.__esModule = true;
-function cssomKeyToCssKey(cssomKey) {
-    if (cssomKey === 'cssFloat') {
-        return 'float';
-    }
-    return cssomKey
-        .replace(/([A-Z])/g, '-$&')
-        .replace(/^(webkit|ms|moz)/, '-')
-        .toLowerCase();
-}
-exports.cssomKeyToCssKey = cssomKeyToCssKey;
-
-
-/***/ }),
 /* 10 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -603,3 +612,4 @@ exports.htmlToSvg = htmlToSvg;
 
 /***/ })
 /******/ ]);
+});
